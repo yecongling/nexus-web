913156da76f913c30a2bb4900fbdb216a8fcf9ac
yeconglin <499475142@qq.com>
2025-03-25 22:05:59 +0800

    🦄 refactor(角色管理): 优化代码

--- src/views/system/Role/AssignRoleMenuDrawer.tsx
+++ src/views/system/Role/AssignRoleMenuDrawer.tsx
@@ -6,126 +6,143 @@ import {
   FolderFilled,
   FolderOpenFilled,
 } from '@ant-design/icons';
+import { useQuery } from '@tanstack/react-query';
 import { Button, Drawer, Space, Tree, type TreeProps } from 'antd';
-import { useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 
 /**
  * 角色菜单授权界面
  * @returns
  */
-const RoleMenuDrawer: React.FC<RoleMenuDrawerProps> = ({
-  open,
-  roleId,
-  onOk,
-  onCancel,
-}) => {
-  // 树组件的数据
-  const [treeData, setTreeData] = useState<any[]>([]);
-  // 选中的节点
-  const [checked, setChecked] = useState<string[]>([]);
-  // 展开的节点
-  const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
+const RoleMenuDrawer: React.FC<RoleMenuDrawerProps> = memo(
+  ({ open, roleId, onOk, onCancel }) => {
+    // 树组件的数据
+    const [treeData, setTreeData] = useState<any[]>([]);
+    // 选中的节点
+    const [checked, setChecked] = useState<string[]>([]);
+    // 展开的节点
+    const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
 
-  useEffect(() => {
-    if (!open) return;
-    // 调用获取所有菜单接口方法（里面包含获取选中的菜单key）
-    roleService.getRoleMenu(roleId).then((resp: any) => {
-      // 内部包含menuList和menuIds
-      const expanded: string[] = [];
-      const data = transformData(resp.menuList, expanded);
-      setTreeData(data);
-      setChecked(resp.menuIds);
-      setExpandedKeys(expanded);
+    // 使用useQuery获取数据
+    const {
+      data: resp,
+      isSuccess,
+      isError,
+      error,
+    } = useQuery({
+      queryKey: ['sys_role_assign_menu', roleId],
+      queryFn: () => roleService.getRoleMenu(roleId),
+      enabled: open,
     });
-  }, [open]);
 
-  /**
-   * 数据转换
-   * @param data 原始数据
-   * @param expanded 需要展开的数据
-   */
-  const transformData = (data: any, expanded: string[]) => {
-    return data.map((item: any) => {
-      if (item.icon) {
-        item.icon = getIcon(item.icon);
+    useEffect(() => {
+      if (isSuccess) {
+        const expanded: string[] = [];
+        const data = transformData(resp.menuList, expanded);
+        setTreeData(data);
+        setChecked(resp.menuIds);
+        setExpandedKeys(expanded);
       }
-      if (item.children?.length > 0) {
-        expanded.push(item.id);
-      }
-      if (item.children) {
-        transformData(item.children, expanded);
-      }
-      return item;
-    });
-  };
+    }, [isSuccess, resp]);
 
-  /**
-   * 点击确定的时候的操作
-   * @param e
-   */
-  const handleOk = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // 保存分配的菜单数据
-    roleService.assignRoleMenu( roleId, checked ).then(() => {
-      // 关闭弹窗
-      onOk(e);
-    });
-  };
+    /**
+     * 数据转换
+     * @param data 原始数据
+     * @param expanded 需要展开的数据
+     */
+    const transformData = useCallback((data: any, expanded: string[]) => {
+      return data.map((item: any) => {
+        if (item.icon) {
+          item.icon = getIcon(item.icon);
+        }
+        if (item.children?.length > 0) {
+          expanded.push(item.id);
+        }
+        if (item.children) {
+          transformData(item.children, expanded);
+        }
+        return item;
+      });
+    }, []);
 
-  /**
-   * 处理节点选中
-   * @param checkedKeys 选中的节点
-   * @param e
-   */
-  const handleChecked: TreeProps['onCheck'] = (checkedKeysValue) => {
-    setChecked((checkedKeysValue as any).checked);
-  };
+    /**
+     * 点击确定的时候的操作
+     * @param e
+     */
+    const handleOk = (e: React.MouseEvent<HTMLButtonElement>) => {
+      // 保存分配的菜单数据
+      roleService.assignRoleMenu(roleId, checked).then(() => {
+        // 关闭弹窗
+        onOk(e);
+      });
+    };
 
-  return (
-    <Drawer
-      title="授权菜单"
-      width={400}
-      open={open}
-      closeIcon={false}
-      extra={<Button type="text" icon={<CloseOutlined />} onClick={onCancel} />}
-      onClose={onCancel}
-      classNames={{ footer: 'text-right' }}
-      footer={
-        <Space>
-          <Button onClick={onCancel}>取消</Button>
-          <Button type="primary" onClick={handleOk}>
-            确认
-          </Button>
-        </Space>
-      }
-    >
-      <Tree
-        blockNode
-        checkable
-        showIcon
-        switcherIcon={<DownOutlined />}
-        defaultExpandAll
-        expandedKeys={expandedKeys}
-        fieldNames={{ title: 'name', key: 'id', children: 'children' }}
-        icon={(props: any) => {
-          // 没有isLeaf这个属性表明是一级菜单
-          const { isLeaf } = props.data;
-          if (!isLeaf) {
-            return props.expanded ? (
-              <FolderOpenFilled style={{ fontSize: '16px', color: 'orange' }} />
-            ) : (
-              <FolderFilled style={{ fontSize: '16px', color: 'orange' }} />
-            );
-          }
-          return <></>;
-        }}
-        treeData={treeData}
-        checkedKeys={checked}
-        checkStrictly
-        onCheck={handleChecked}
-      />
-    </Drawer>
-  );
-};
+    /**
+     * 处理节点选中
+     * @param checkedKeys 选中的节点
+     * @param e
+     */
+    const handleChecked: TreeProps['onCheck'] = useCallback(
+      (checkedKeysValue: any) => {
+        setChecked((checkedKeysValue as any).checked);
+      },
+      [],
+    );
+
+    return (
+      <Drawer
+        title="授权菜单"
+        width={400}
+        open={open}
+        closeIcon={false}
+        extra={
+          <Button type="text" icon={<CloseOutlined />} onClick={onCancel} />
+        }
+        onClose={onCancel}
+        classNames={{ footer: 'text-right' }}
+        footer={
+          <Space>
+            <Button onClick={onCancel}>取消</Button>
+            <Button type="primary" onClick={handleOk}>
+              确认
+            </Button>
+          </Space>
+        }
+      >
+        {isError && <div>数据加载失败，{error.message}</div>}
+        {isSuccess && (
+          <Tree
+            blockNode
+            checkable
+            showIcon
+            switcherIcon={<DownOutlined />}
+            defaultExpandAll
+            expandedKeys={expandedKeys}
+            fieldNames={{ title: 'name', key: 'id', children: 'children' }}
+            icon={(props: any) => {
+              // 没有isLeaf这个属性表明是一级菜单
+              const { isLeaf } = props.data;
+              if (!isLeaf) {
+                return props.expanded ? (
+                  <FolderOpenFilled
+                    style={{ fontSize: '16px', color: 'orange' }}
+                  />
+                ) : (
+                  <FolderFilled style={{ fontSize: '16px', color: 'orange' }} />
+                );
+              }
+              return <></>;
+            }}
+            treeData={treeData}
+            checkedKeys={checked}
+            checkStrictly
+            onCheck={handleChecked}
+          />
+        )}
+      </Drawer>
+    );
+  },
+);
 export default RoleMenuDrawer;
 
 export type RoleMenuDrawerProps = {
--- src/views/system/Role/AssignRoleUserDrawer.tsx
+++ src/views/system/Role/AssignRoleUserDrawer.tsx
@@ -26,7 +26,7 @@ import {
   Table,
   type TableProps,
 } from 'antd';
-import { useRef, useState } from 'react';
+import { memo, useRef, useState } from 'react';
 import AddUser from './AddUserModal';
 import { roleService } from './api/roleApi';
 import type { UserSearchParams } from './api/type';
@@ -36,345 +36,343 @@ import { useQuery } from '@tanstack/react-query';
  * 给角色分配用户
  * @returns
  */
-const RoleUserDrawer: React.FC<RoleUserDrawerProps> = ({
-  open,
-  roleId,
-  onCancel,
-}) => {
-  const { modal } = App.useApp();
-  // 添加用户弹窗的打开关闭
-  const [openAddUser, setOpenAddUser] = useState<boolean>(false);
-  // 检索表单
-  const [form] = Form.useForm();
-  // 当前选中的行数据
-  const [selRows, setSelectedRows] = useState<any[]>([]);
-  // 第一个检索框
-  const ref = useRef<InputRef>(null);
+const RoleUserDrawer: React.FC<RoleUserDrawerProps> = memo(
+  ({ open, roleId, onCancel }) => {
+    const { modal } = App.useApp();
+    // 添加用户弹窗的打开关闭
+    const [openAddUser, setOpenAddUser] = useState<boolean>(false);
+    // 检索表单
+    const [form] = Form.useForm();
+    // 当前选中的行数据
+    const [selRows, setSelectedRows] = useState<any[]>([]);
+    // 第一个检索框
+    const ref = useRef<InputRef>(null);
 
-  // 查询参数（包含分页参数）
-  const [searchParams, setSearchParams] = useState<UserSearchParams>({
-    pageNum: 1,
-    pageSize: 20,
-  });
-
-  // 查询用户数据
-  const { isLoading, data, refetch } = useQuery({
-    queryKey: ['sys_role_users_drawer', [roleId, searchParams]],
-    queryFn: () => roleService.getRoleUser(roleId, searchParams),
-    enabled: open,
-  });
-
-  /**
-   * 定义表格的列
-   */
-  const columns: TableProps['columns'] = [
-    {
-      title: 'id',
-      dataIndex: 'id',
-      hidden: true,
-    },
-    {
-      title: '用户ID',
-      dataIndex: 'userId',
-      width: 80,
-      align: 'center',
-      hidden: true,
-    },
-    {
-      title: '用户名',
-      dataIndex: 'username',
-      width: 80,
-      align: 'left',
-    },
-    {
-      title: '实名',
-      dataIndex: 'realName',
-      width: 80,
-      align: 'left',
-    },
-    {
-      title: '性别',
-      dataIndex: 'sex',
-      width: 80,
-      align: 'center',
-      render: (text) => {
-        return text === 1 ? (
-          <ManOutlined className="text-blue-400" />
-        ) : (
-          <WomanOutlined className="text-pink-400" />
-        );
-      },
-    },
-    {
-      title: '操作',
-      dataIndex: 'action',
-      width: 80,
-      fixed: 'right',
-      align: 'center',
-      render: (_text, record) => {
-        return (
-          <Popconfirm
-            title="移除用户"
-            description="确定从该角色下移除当前用户吗？"
-            onConfirm={() => deleteRoleUser(record.id)}
-            icon={<WarningOutlined style={{ color: 'red' }} />}
-          >
-            <Button type="link" danger size="small">
-              移除
-            </Button>
-          </Popconfirm>
-        );
-      },
-    },
-  ];
-
-  /**
-   * 分页改变事件
-   * @param page 页数
-   * @param pageSize 每页数量
-   */
-  const onPageSizeChange = (page: number, pageSize: number) => {
-    setSearchParams({
-      ...searchParams,
-      pageNum: page,
-      pageSize: pageSize,
+    // 查询参数（包含分页参数）
+    const [searchParams, setSearchParams] = useState<UserSearchParams>({
+      pageNum: 1,
+      pageSize: 20,
     });
-  };
 
-  /**
-   * 表单检索
-   */
-  const onFinish = (values: UserSearchParams) => {
-    const search = {
-      ...values,
-      pageNum: searchParams.pageNum,
-      pageSize: searchParams.pageSize,
-    };
-    // 判断参数是否发生变化
-    if (JSON.stringify(search) === JSON.stringify(searchParams)) {
-      // 参数没有变化，手动刷新数据
-      refetch();
-      return;
-    }
-    setSearchParams((prev) => ({ ...prev, ...search }));
-  };
-
-  /**
-   * 多行选中的配置
-   */
-  const rowSelection: TableProps['rowSelection'] = {
-    // 行选中的回调
-    onChange(_selectedRowKeys, selectedRows) {
-      setSelectedRows(selectedRows);
-    },
-    columnWidth: 32,
-    fixed: true,
-    selectedRowKeys: selRows.map((item) => item.id),
-  };
-
-  /**
-   * 打开添加用户弹窗
-   */
-  const addUser = () => {
-    setOpenAddUser(true);
-  };
-
-  /**
-   * 取消添加用户
-   */
-  const cancelAddUser = () => {
-    setOpenAddUser(false);
-    ref.current?.focus();
-  };
-
-  /**
-   * 删除单个用户
-   * @param id 用户ID
-   */
-  const deleteRoleUser = (id: string) => {
-    roleService.assignRoleUser(roleId, [id], 'remove').then(() => {
-      // 刷新表格数据
-      refetch();
-      // 清空选择项
-      setSelectedRows([]);
+    // 查询用户数据
+    const { isLoading, data, refetch } = useQuery({
+      queryKey: ['sys_role_users_drawer', [roleId, searchParams]],
+      queryFn: () => roleService.getRoleUser(roleId, searchParams),
+      enabled: open,
     });
-  };
 
-  /**
-   * 批量删除用户
-   * @param id 用户ID
-   */
-  const deleteBatch = (id?: string) => {
-    // 删除操作需要二次确定
-    modal.confirm({
-      title: '删除用户',
-      icon: <ExclamationCircleFilled />,
-      content: '确定删除用户吗？数据删除后将无法恢复！',
-      onOk() {
-        // 调用删除接口，删除成功后刷新页面数据
-        const ids = selRows.map((item: any) => item.id);
-        id && ids.push(id);
-        roleService.assignRoleUser(roleId, ids, 'remove').then(() => {
-          // 刷新表格数据
-          refetch();
-          // 清空选择项
-          setSelectedRows([]);
-        });
+    /**
+     * 定义表格的列
+     */
+    const columns: TableProps['columns'] = [
+      {
+        title: 'id',
+        dataIndex: 'id',
+        hidden: true,
       },
-    });
-  };
-
-  /**
-   * 处理确定按钮的点击事件
-   * @param count 选中的数量
-   */
-  const handleOk = (count: number) => {
-    // 如果选中的数量为0，则直接关闭弹窗，不刷新表格，否则刷新表格
-    if (count === 0) {
-      cancelAddUser();
-      return;
-    }
-    refetch();
-    cancelAddUser();
-  };
-
-  return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Form: {
-            itemMarginBottom: 0,
-          },
+      {
+        title: '用户ID',
+        dataIndex: 'userId',
+        width: 80,
+        align: 'center',
+        hidden: true,
+      },
+      {
+        title: '用户名',
+        dataIndex: 'username',
+        width: 80,
+        align: 'left',
+      },
+      {
+        title: '实名',
+        dataIndex: 'realName',
+        width: 80,
+        align: 'left',
+      },
+      {
+        title: '性别',
+        dataIndex: 'sex',
+        width: 80,
+        align: 'center',
+        render: (text) => {
+          return text === 1 ? (
+            <ManOutlined className="text-blue-400" />
+          ) : (
+            <WomanOutlined className="text-pink-400" />
+          );
         },
-      }}
-    >
-      <Drawer
-        title="分配用户"
-        width={920}
-        open={open}
-        closeIcon={false}
-        extra={
-          <Button type="text" icon={<CloseOutlined />} onClick={onCancel} />
-        }
-        onClose={onCancel}
-        classNames={{ footer: 'text-right', body: 'flex flex-col' }}
-      >
-        <Card className="mb-2!">
-          <Form form={form} onFinish={onFinish}>
-            <Row gutter={12}>
-              <Col span={6}>
-                <Form.Item
-                  className="mb-0"
-                  name="username"
-                  label="用户名"
-                  colon={false}
-                >
-                  <Input autoFocus allowClear autoComplete="off" ref={ref} />
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                <Form.Item
-                  className="mb-0"
-                  name="realName"
-                  label="实际名"
-                  colon={false}
-                >
-                  <Input allowClear autoComplete="off" />
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                <Form.Item
-                  className="mb-0"
-                  name="sex"
-                  label="性别"
-                  colon={false}
-                >
-                  <Select
-                    allowClear
-                    options={[
-                      { value: '', label: '请选择', disabled: true },
-                      { value: 1, label: '男' },
-                      { value: 0, label: '女' },
-                    ]}
-                  />
-                </Form.Item>
-              </Col>
-              <Col span={6} style={{ textAlign: 'right' }}>
-                <Space>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    icon={<SearchOutlined />}
-                  >
-                    检索
-                  </Button>
-                  <Button
-                    type="default"
-                    icon={<RedoOutlined />}
-                    onClick={() => {
-                      form.resetFields();
-                    }}
-                  >
-                    重置
-                  </Button>
-                </Space>
-              </Col>
-            </Row>
-          </Form>
-        </Card>
-        <Card
-          className="mt-2 flex-1 min-h-0"
-          styles={{ body: { height: '100%' } }}
-        >
-          <Space>
-            <Button type="primary" onClick={addUser} icon={<PlusOutlined />}>
-              添加用户
-            </Button>
-            <Button
-              icon={<DeleteOutlined />}
-              danger
-              disabled={selRows.length === 0}
-              onClick={() => deleteBatch()}
+      },
+      {
+        title: '操作',
+        dataIndex: 'action',
+        width: 80,
+        fixed: 'right',
+        align: 'center',
+        render: (_text, record) => {
+          return (
+            <Popconfirm
+              title="移除用户"
+              description="确定从该角色下移除当前用户吗？"
+              onConfirm={() => deleteRoleUser(record.id)}
+              icon={<WarningOutlined style={{ color: 'red' }} />}
             >
-              批量删除
-            </Button>
-          </Space>
-          {/* 表格数据 */}
-          <Table
-            className="mt-2"
-            size="small"
-            columns={columns}
-            dataSource={data?.data || []}
-            loading={isLoading}
-            bordered
-            rowKey="id"
-            pagination={{
-              pageSize: searchParams.pageSize,
-              current: searchParams.pageNum,
-              showQuickJumper: true,
-              hideOnSinglePage: false,
-              showSizeChanger: true,
-              showTotal: (total) => `共 ${total} 条`,
-              total: data?.total,
-              onChange(page, pageSize) {
-                onPageSizeChange(page, pageSize);
-              },
-            }}
-            scroll={{ x: 'max-content' }}
-            rowSelection={{ ...rowSelection }}
-          />
-        </Card>
-      </Drawer>
-      {/* 添加用户弹窗 */}
-      <AddUser
-        roleId={roleId}
-        open={openAddUser}
-        onCancel={cancelAddUser}
-        onOk={handleOk}
-      />
-    </ConfigProvider>
-  );
-};
+              <Button type="link" danger size="small">
+                移除
+              </Button>
+            </Popconfirm>
+          );
+        },
+      },
+    ];
+
+    /**
+     * 分页改变事件
+     * @param page 页数
+     * @param pageSize 每页数量
+     */
+    const onPageSizeChange = (page: number, pageSize: number) => {
+      setSearchParams({
+        ...searchParams,
+        pageNum: page,
+        pageSize: pageSize,
+      });
+    };
+
+    /**
+     * 表单检索
+     */
+    const onFinish = (values: UserSearchParams) => {
+      const search = {
+        ...values,
+        pageNum: searchParams.pageNum,
+        pageSize: searchParams.pageSize,
+      };
+      // 判断参数是否发生变化
+      if (JSON.stringify(search) === JSON.stringify(searchParams)) {
+        // 参数没有变化，手动刷新数据
+        refetch();
+        return;
+      }
+      setSearchParams((prev) => ({ ...prev, ...search }));
+    };
+
+    /**
+     * 多行选中的配置
+     */
+    const rowSelection: TableProps['rowSelection'] = {
+      // 行选中的回调
+      onChange(_selectedRowKeys, selectedRows) {
+        setSelectedRows(selectedRows);
+      },
+      columnWidth: 32,
+      fixed: true,
+      selectedRowKeys: selRows.map((item) => item.id),
+    };
+
+    /**
+     * 打开添加用户弹窗
+     */
+    const addUser = () => {
+      setOpenAddUser(true);
+    };
+
+    /**
+     * 取消添加用户
+     */
+    const cancelAddUser = () => {
+      setOpenAddUser(false);
+      ref.current?.focus();
+    };
+
+    /**
+     * 删除单个用户
+     * @param id 用户ID
+     */
+    const deleteRoleUser = (id: string) => {
+      roleService.assignRoleUser(roleId, [id], 'remove').then(() => {
+        // 刷新表格数据
+        refetch();
+        // 清空选择项
+        setSelectedRows([]);
+      });
+    };
+
+    /**
+     * 批量删除用户
+     * @param id 用户ID
+     */
+    const deleteBatch = (id?: string) => {
+      // 删除操作需要二次确定
+      modal.confirm({
+        title: '删除用户',
+        icon: <ExclamationCircleFilled />,
+        content: '确定删除用户吗？数据删除后将无法恢复！',
+        onOk() {
+          // 调用删除接口，删除成功后刷新页面数据
+          const ids = selRows.map((item: any) => item.id);
+          id && ids.push(id);
+          roleService.assignRoleUser(roleId, ids, 'remove').then(() => {
+            // 刷新表格数据
+            refetch();
+            // 清空选择项
+            setSelectedRows([]);
+          });
+        },
+      });
+    };
+
+    /**
+     * 处理确定按钮的点击事件
+     * @param count 选中的数量
+     */
+    const handleOk = (count: number) => {
+      // 如果选中的数量为0，则直接关闭弹窗，不刷新表格，否则刷新表格
+      if (count === 0) {
+        cancelAddUser();
+        return;
+      }
+      refetch();
+      cancelAddUser();
+    };
+
+    return (
+      <ConfigProvider
+        theme={{
+          components: {
+            Form: {
+              itemMarginBottom: 0,
+            },
+          },
+        }}
+      >
+        <Drawer
+          title="分配用户"
+          width={920}
+          open={open}
+          closeIcon={false}
+          extra={
+            <Button type="text" icon={<CloseOutlined />} onClick={onCancel} />
+          }
+          onClose={onCancel}
+          classNames={{ footer: 'text-right', body: 'flex flex-col' }}
+        >
+          <Card className="mb-2!">
+            <Form form={form} onFinish={onFinish}>
+              <Row gutter={12}>
+                <Col span={6}>
+                  <Form.Item
+                    className="mb-0"
+                    name="username"
+                    label="用户名"
+                    colon={false}
+                  >
+                    <Input autoFocus allowClear autoComplete="off" ref={ref} />
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item
+                    className="mb-0"
+                    name="realName"
+                    label="实际名"
+                    colon={false}
+                  >
+                    <Input allowClear autoComplete="off" />
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item
+                    className="mb-0"
+                    name="sex"
+                    label="性别"
+                    colon={false}
+                  >
+                    <Select
+                      allowClear
+                      options={[
+                        { value: '', label: '请选择', disabled: true },
+                        { value: 1, label: '男' },
+                        { value: 0, label: '女' },
+                      ]}
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={6} style={{ textAlign: 'right' }}>
+                  <Space>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      icon={<SearchOutlined />}
+                    >
+                      检索
+                    </Button>
+                    <Button
+                      type="default"
+                      icon={<RedoOutlined />}
+                      onClick={() => {
+                        form.resetFields();
+                      }}
+                    >
+                      重置
+                    </Button>
+                  </Space>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
+          <Card
+            className="mt-2 flex-1 min-h-0"
+            styles={{ body: { height: '100%' } }}
+          >
+            <Space>
+              <Button type="primary" onClick={addUser} icon={<PlusOutlined />}>
+                添加用户
+              </Button>
+              <Button
+                icon={<DeleteOutlined />}
+                danger
+                disabled={selRows.length === 0}
+                onClick={() => deleteBatch()}
+              >
+                批量删除
+              </Button>
+            </Space>
+            {/* 表格数据 */}
+            <Table
+              className="mt-2"
+              size="small"
+              columns={columns}
+              dataSource={data?.data || []}
+              loading={isLoading}
+              bordered
+              rowKey="id"
+              pagination={{
+                pageSize: searchParams.pageSize,
+                current: searchParams.pageNum,
+                showQuickJumper: true,
+                hideOnSinglePage: false,
+                showSizeChanger: true,
+                showTotal: (total) => `共 ${total} 条`,
+                total: data?.total,
+                onChange(page, pageSize) {
+                  onPageSizeChange(page, pageSize);
+                },
+              }}
+              scroll={{ x: 'max-content' }}
+              rowSelection={{ ...rowSelection }}
+            />
+          </Card>
+        </Drawer>
+        {/* 添加用户弹窗 */}
+        <AddUser
+          roleId={roleId}
+          open={openAddUser}
+          onCancel={cancelAddUser}
+          onOk={handleOk}
+        />
+      </ConfigProvider>
+    );
+  },
+);
 
 export default RoleUserDrawer;
 
