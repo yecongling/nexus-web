ea75bccd99ec222863c24d2862a06208813135a2
ycl <499475142@qq.com>
2025-03-28 17:36:40 +0800

    ✨ feat(数据字典): 文件添加、模型添加

--- src/views/system/Dict/api/type.d.ts
+++ src/views/system/Dict/api/type.d.ts
@@ -1,3 +1,6 @@
+/**
+ * @description: 数据字典模块检索参数
+ */
 export interface DictSearchParams {
   // 字典名称
   dictName?: string;
@@ -5,4 +8,18 @@ export interface DictSearchParams {
   dictCode?: string;
   pageNum: number;
   pageSize: number;
+}
+
+/**
+ * 字典模型
+ */
+export interface DictModel {
+  id: string;
+}
+
+/**
+ * 字典项模型
+ */
+export interface DictItemModel {
+  id: string;
 }
