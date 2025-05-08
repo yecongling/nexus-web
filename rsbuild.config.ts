ae84c7177eebdf95c6d801174556717c5e7cceb6
ycl <499475142@qq.com>
2025-05-08 13:58:44 +0800

    🔧 build(rsbuild依赖): 升级依赖并调整html模板

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -36,14 +36,7 @@ export default defineConfig({
   ],
   // 配置html模板
   html: {
-    favicon: path.resolve(__dirname, './src/assets/images/favicon.ico'),
-    title: 'flex-fusion',
-    tags: [
-      {
-        tag: 'html',
-        attrs: { lang: 'zh' },
-      },
-    ],
+    template: './index.html',
   },
   // 配置路径别名
   source: {
@@ -73,6 +66,7 @@ export default defineConfig({
         'rc-cp': /node_modules[\\/]rc-/,
       },
     },
+    buildCache: true,
     // 移除console.[method]语句
     removeConsole: true,
     // 开启包文件分析
