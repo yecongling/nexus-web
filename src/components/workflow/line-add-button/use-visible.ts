fa6236db4e34d58ff4664ea5f4b0a7041da13452
ycl <499475142@qq.com>
2025-06-09 20:32:03 +0800

    ✨ feat(流程编排-线条设置): 线条可见行判定方法

--- src/components/workflow/line-add-button/use-visible.ts
+++ src/components/workflow/line-add-button/use-visible.ts
@@ -1,13 +1,27 @@
-import type { WorkflowLineEntity } from "@flowgram.ai/free-layout-editor"
+import {
+  usePlayground,
+  type WorkflowLineEntity,
+} from '@flowgram.ai/free-layout-editor';
 
 /**
  * 判断连线中间内容是否可见
- * @param params 
+ * @param params
  */
 export const useVisible = (params: {
-    line: WorkflowLineEntity,
-    selected?: boolean;
-    hovered?: boolean;
+  line: WorkflowLineEntity;
+  selected?: boolean;
+  hovered?: boolean;
 }): boolean => {
-
-}   
+  const playground = usePlayground();
+  const { line, selected = false, hovered } = params;
+  if (line.disposed) {
+    return false;
+  }
+  if (playground.config.readonly) {
+    return false;
+  }
+  if (!selected && !hovered) {
+    return false;
+  }
+  return true;
+};
