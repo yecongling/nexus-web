6c06efd357bdc2f4f6919473870410f1b2d9b4a2
ycl <499475142@qq.com>
2025-06-20 18:06:37 +0800

    ✨ feat(应用中心-标签选择): 标签选择面板完善

--- src/components/base/tag-management/selector.tsx
+++ src/components/base/tag-management/selector.tsx
@@ -1,8 +1,10 @@
 import type { HtmlContentProps } from '@/components/popover';
+import CustomPopover from '@/components/popover';
 import { tagsService } from '@/services/common/tags/tagsApi';
 import type { Tag } from '@/services/common/tags/tagsModel';
 import { useTagStore } from '@/stores/useTagStore';
 import cn from '@/utils/classnames';
+import { TagOutlined } from '@ant-design/icons';
 import type React from 'react';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -73,16 +75,50 @@ const TagSelector: React.FC<TagSelectorProps> = ({
    * 触发器内容
    * @returns 触发器内容
    */
-  const trigger = () => {
+  const Trigger = () => {
     return (
-      <div className={cn(
-        'relative'
-      )}>
-
+      <div
+        className={cn(
+          'relative flex w-full cursor-pointer items-center gap-1 rounded-md px-2 py-[7px] hover:bg-[#c8ceda33]',
+        )}
+      >
+        <TagOutlined className="h-3 w-3 shrink-0" />
+        <div className="text-[#98a2b2] grow truncate text-start text-[13px] font-[400] leading-4">
+          {!triggerContent ? t('common.tag.addTag') : triggerContent}
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
-  return <div>标签选择器</div>;
+  return (
+    <>
+      {isPopover && (
+        <CustomPopover
+          htmlContent={
+            <Panel
+              type={type}
+              targetID={targetID}
+              value={value}
+              selectedTags={selectedTags}
+              onCacheUpdate={onCacheUpdate}
+              onChange={onChange}
+              onCreate={getTagList}
+            />
+          }
+          position={position}
+          trigger="click"
+          btnElement={<Trigger />}
+          btnClassName={(open) =>
+            cn(
+              open ? '!bg-[#c8ceda33] !text-[#101828]' : '!bg-transparent',
+              '!w-full !border-0 !p-0 !text-[#101828] hover:!bg-[#c8ceda33] hover:!text-[#101828]',
+            )
+          }
+          popupClassName="!w-full !ring-0"
+          className="!z-20 h-fit !w-full"
+        />
+      )}
+    </>
+  );
 };
 export default TagSelector;
